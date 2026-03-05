bbd3ce2d19fa9e3ff1c8b34ba136fcf876337f80
Akash <2025krishnaram@gmail.com>
2026-03-05 18:39:41 +0530

    up

--- Code.py
+++ Code.py
@@ -3,10 +3,10 @@ import speech_recognition as sr
 
 def speechtxt(x):
     engine=pyttsx3.init()
-    voices = engine.getProperty('voices')#here this get the voice from the engine.
-    engine.setProperty('voice',voices[0].id)#here its set the voice and 0 is for male voice and 1 is for female voice.
-    rate =  engine.getProperty('rate')#here get the 'rate' form the eniginee for the speed of the voice.
-    engine.setProperty('rate',140)#ste the speet of the voiec.
-    engine.say(x)#for speaking the text using the x variable.
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice',voices[0].id)
+    rate =  engine.getProperty('rate')
+    engine.setProperty('rate',140)
+    engine.say(x)
     engine.runAndWait()
 speechtxt("Nice to Meeting You")
